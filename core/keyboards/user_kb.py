b6f9ca581b143ed9f31bca4a7aae80776d317968
Daniil <Palona2006@yandex.ru>
2024-02-08 23:17:33 +0900

    bug with empty menu kb fixed Bug happen because in delete_button used global variable and changed it.

--- core/keyboards/user_kb.py
+++ core/keyboards/user_kb.py
@@ -88,10 +88,11 @@ complex_dinner = InlineKeyboardMarkup(inline_keyboard=[
 
 
 async def delete_button(keyboard: ReplyKeyboardMarkup, already_pressed: str) -> ReplyKeyboardMarkup:
+    new_markup = keyboard
     already_pressed = already_pressed.split(' ')
     for text in already_pressed:
         try:
-            keyboard.keyboard[0].remove(KeyboardButton(text=text))
+            new_markup.keyboard[0].remove(KeyboardButton(text=text))
         except ValueError:
             pass
-    return keyboard
+    return new_markup
